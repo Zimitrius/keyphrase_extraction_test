6a42b50fd33cc53ce18851113f36469022d53bda
Zimitrius <48524616+Zimitrius@users.noreply.github.com>
2021-01-21 21:50:18 +0200

    final...

--- keyphrase_extract.py
+++ keyphrase_extract.py
@@ -20,15 +20,15 @@ def get_keyphrase(user_text, unic_filename): # get keyphrase from text end send 
 def keyphrase_saver(key_list, un_id): #save phrase list and chekc if exist wikipedia page for every single keyphrase
 	with shelve.open(f"list_{un_id}") as keys:
 		for key,score in key_list:
-			existing  = check_wiki_page_exst(key)
-			keys[key] = (key, existing)
+			wiki_exist = check_wiki_page_exst(key)
+			keys[key]  = (score, wiki_exist)
 	update_top_list(key_list)
 
 def update_top_list(doc): # add keyphrase to top list or update it position if phrase already exist
 	with shelve.open('top_list') as top:
 		for key, _ in doc:
 			top[key] = top.get(key, 0) + 1
-			#print(key, top[key])
+			#print(key, top[key], _)
 
 def check_wiki_page_exst(word): # check if keyphrase exist and return bool condition
 	wiki_wiki  = wikipediaapi.Wikipedia('en')
