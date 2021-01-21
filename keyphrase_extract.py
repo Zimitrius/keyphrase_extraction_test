0ebda0d3c3f9a425c1a0eef3bf150b99eecea084
Zimitrius <48524616+Zimitrius@users.noreply.github.com>
2021-01-21 21:16:51 +0200

    fix bug

--- keyphrase_extract.py
+++ keyphrase_extract.py
@@ -22,7 +22,7 @@ def keyphrase_saver(key_list): #save phrase list and chekc if exist wikipedia pa
 	filename = f"text_{uuid.uuid4()}"
 	with shelve.open(filename) as keys:
 		for key,score in key_list:
-			existing = check_wiki_page_exst(key)
+			existing  = check_wiki_page_exst(key)
 			keys[key] = (key, existing)
 	update_top_list(key_list)
 
@@ -33,9 +33,9 @@ def update_top_list(doc): # add keyphrase to top list or update it position if p
 			#print(key, top[key])
 
 def check_wiki_page_exst(word): # check if keyphrase exist and return bool condition
-	wiki_wiki = wikipediaapi.Wikipedia('en')
-	page_py = wiki_wiki.page(word)
-	status = page_py.exists()
+	wiki_wiki  = wikipediaapi.Wikipedia('en')
+	page_check = wiki_wiki.page(word)
+	status     = page_check.exists()
 	#print(f"Page - Exists: {status} - {word}")
 	return status
 
